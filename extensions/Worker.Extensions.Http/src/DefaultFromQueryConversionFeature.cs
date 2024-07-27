cf7ee059ffd1c2d7bec8ab4056834a2d6727dc54
Erin McLaughlin <erinmclaughlin219@gmail.com>
2024-07-27 19:25:43 -0400

    pull query string utilities into its own class

--- extensions/Worker.Extensions.Http/src/DefaultFromQueryConversionFeature.cs
+++ extensions/Worker.Extensions.Http/src/DefaultFromQueryConversionFeature.cs
@@ -2,13 +2,6 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Globalization;
-using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker.Extensions.Http.Converters;
 using Microsoft.Azure.Functions.Worker.Http;
@@ -51,127 +44,18 @@ namespace Microsoft.Azure.Functions.Worker.Extensions.Http
 
         private static object? ConvertQuery(FunctionContext context, HttpRequestData requestData, Type targetType, object? source)
         {
-            if (IsSimpleType(targetType))
+            if (QueryStringUtilities.TryConvertToType(source, targetType, out var result))
             {
-                return source is not null ? ConvertSimpleType(targetType, source) : CreateDefaultInstanceOfType(targetType);
+                return result;
             }
 
-            return DeserializeQuery(context, targetType, requestData.Query);
-        }
-
-        private static object? DeserializeQuery(FunctionContext context, Type targetType, NameValueCollection query)
-        {
             var serializer = context.InstanceServices.GetService<IOptions<WorkerOptions>>()?.Value?.Serializer
                 ?? throw new InvalidOperationException("A serializer is not configured for the worker.");
 
-            // Convert query string to dictionary type:
-            var queryParamsAsDictionary = GetObjectDictionary(targetType, query);
+            var queryParamsAsDictionary = requestData.Query.ToObjectDictionary(targetType);
 
-            // Use object serializer to convert from dictionary to target type:
             var stream = serializer.Serialize(queryParamsAsDictionary).ToStream();
             return serializer.Deserialize(stream, targetType, context.CancellationToken);
-        }
-
-        private static Dictionary<string, object?> GetObjectDictionary(Type targetType, NameValueCollection query)
-        {
-            var dict = new Dictionary<string, object?>();
-            const BindingFlags bindingFlags = BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance;
-
-            foreach (var propertyName in query.AllKeys)
-            {
-                // Skip if the target type does not contain a property with a matching name:
-                if (string.IsNullOrEmpty(propertyName) || targetType.GetProperty(propertyName, bindingFlags)?.PropertyType is not { } propertyType)
-                {
-                    continue;
-                }
-
-                // Handle simple types:
-                if (IsSimpleType(propertyType))
-                {
-                    dict.Add(propertyName, ConvertSimpleType(propertyType, query[propertyName]));
-                    continue;
-                }
-                
-                // Handle collection types:
-                if (typeof(IEnumerable).IsAssignableFrom(propertyType))
-                {
-                    var arrayType = GetArrayType(propertyType);
-                    var arrayValues = query.GetValues(propertyName);
-
-                    // Handle collections of simple types:
-                    if (IsSimpleType(arrayType))
-                    {
-                        var parsedValues = arrayValues.Select(p => ConvertSimpleType(arrayType, p)).ToArray();
-                        dict.Add(propertyName, parsedValues);
-                    }
-
-                    // TODO: Handle collections of complex types
-                    // ..
-                    
-                    continue;
-                }
-
-                // TODO: Handle complex types
-                // ..
-            }
-
-            return dict;
-        }
-
-        private static object? CreateDefaultInstanceOfType(Type targetType)
-        {
-            return targetType.IsValueType ? Activator.CreateInstance(targetType) : null;
-        }
-
-        private static object? ConvertSimpleType(Type targetType, object? valueToConvert)
-        {
-            var stringValue = valueToConvert?.ToString();
-
-            if (targetType == typeof(string))
-            {
-                return stringValue;
-            }
-
-            if (string.IsNullOrEmpty(stringValue))
-            {
-                return CreateDefaultInstanceOfType(targetType);
-            }
-
-            var typeConverter = TypeDescriptor.GetConverter(targetType);
-
-            if (typeConverter.IsValid(stringValue))
-            {
-                return typeConverter.ConvertFromString(null, CultureInfo.InvariantCulture, stringValue);
-            }
-
-            return CreateDefaultInstanceOfType(targetType);
-        }
-
-        private static Type GetArrayType(Type propertyType)
-        {
-            return propertyType.GetElementType() ?? propertyType.GenericTypeArguments[0];
-        }
-
-        private static bool IsSimpleType(Type type)
-        {
-            if (Nullable.GetUnderlyingType(type) is { } underlyingType)
-            {
-                type = underlyingType;
-            }
-
-            return type.IsPrimitive
-                || type.IsEnum
-                || type == typeof(string)
-                || type == typeof(decimal)
-                || type == typeof(DateTime)
-                || type == typeof(DateTimeOffset)
-                || type == typeof(TimeSpan)
-                || type == typeof(Guid);
-        }
-
-        private static bool IsNullableType(Type type)
-        {
-            return Nullable.GetUnderlyingType(type) != null;
         }
     }
 }
---
+++ extensions/Worker.Extensions.Http/src/QueryStringUtilities.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+
+namespace Microsoft.Azure.Functions.Worker.Extensions.Http;
+
+internal static class QueryStringUtilities
+{
+    public static bool TryConvertToType(object? source, Type targetType, out object? result)
+    {
+        if (IsSimpleType(targetType))
+        {
+            result = source is not null ? ConvertSimpleType(targetType, source) : CreateDefaultInstanceOfType(targetType);
+            return true;
+        }
+
+        result = null;
+        return false;
+    }
+
+    public static Dictionary<string, object?> ToObjectDictionary(this NameValueCollection query, Type targetType)
+    {
+        var dict = new Dictionary<string, object?>();
+        const BindingFlags bindingFlags = BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance;
+
+        foreach (var propertyName in query.AllKeys)
+        {
+            // Skip if the target type does not contain a property with a matching name:
+            if (string.IsNullOrEmpty(propertyName) || targetType.GetProperty(propertyName, bindingFlags)?.PropertyType is not { } propertyType)
+            {
+                continue;
+            }
+
+            // Handle simple types:
+            if (IsSimpleType(propertyType))
+            {
+                dict.Add(propertyName, ConvertSimpleType(propertyType, query[propertyName]));
+                continue;
+            }
+
+            // Handle collection types:
+            if (typeof(IEnumerable).IsAssignableFrom(propertyType))
+            {
+                var arrayType = GetArrayType(propertyType);
+                var arrayValues = query.GetValues(propertyName);
+
+                // Handle collections of simple types:
+                if (IsSimpleType(arrayType))
+                {
+                    var parsedValues = arrayValues.Select(p => ConvertSimpleType(arrayType, p)).ToArray();
+                    dict.Add(propertyName, parsedValues);
+                }
+
+                // TODO: Handle collections of complex types
+                // ..
+
+                continue;
+            }
+
+            // TODO: Handle complex types
+            // ..
+        }
+
+        return dict;
+    }
+
+    private static object? ConvertSimpleType(Type targetType, object? valueToConvert)
+    {
+        var stringValue = valueToConvert?.ToString();
+
+        if (targetType == typeof(string))
+        {
+            return stringValue;
+        }
+
+        if (string.IsNullOrEmpty(stringValue))
+        {
+            return CreateDefaultInstanceOfType(targetType);
+        }
+
+        var typeConverter = TypeDescriptor.GetConverter(targetType);
+
+        if (typeConverter.IsValid(stringValue))
+        {
+            return typeConverter.ConvertFromString(null, CultureInfo.InvariantCulture, stringValue);
+        }
+
+        return CreateDefaultInstanceOfType(targetType);
+    }
+
+    private static object? CreateDefaultInstanceOfType(Type targetType)
+    {
+        return targetType.IsValueType ? Activator.CreateInstance(targetType) : null;
+    }
+
+    private static Type GetArrayType(Type propertyType)
+    {
+        return propertyType.GetElementType() ?? propertyType.GenericTypeArguments[0];
+    }
+
+    private static bool IsSimpleType(Type type)
+    {
+        if (Nullable.GetUnderlyingType(type) is { } underlyingType)
+        {
+            type = underlyingType;
+        }
+
+        return type.IsPrimitive
+            || type.IsEnum
+            || type == typeof(string)
+            || type == typeof(decimal)
+            || type == typeof(DateTime)
+            || type == typeof(DateTimeOffset)
+            || type == typeof(TimeSpan)
+            || type == typeof(Guid);
+    }
+}
